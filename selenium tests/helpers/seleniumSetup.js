62e9e4c0570696dcaff55618b15a1963ea14a83f
Justin Kurdila <justinkurdila@gmail.com>
2024-08-02 16:24:05 -0400

    fixing browsers by adding env variable

--- selenium tests/helpers/seleniumSetup.js
+++ selenium tests/helpers/seleniumSetup.js
@@ -1,26 +1,48 @@
 const { Builder, By } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 require('chromedriver');
+require('geckodriver');
 
 // Constants
 const BASE_URL = 'https://demoqa.com/';
 const TIMEOUT = 30000;
 const SELENIUM_SERVER_URL = process.env.SELENIUM_SERVER_URL || '';
 const HEADLESS = process.env.HEADLESS === 'true';
+const BROWSER = process.env.SELENIUM_BROWSER || 'chrome';
 
-// Chrome Options
+// Browser Options
 const chromeOptions = new chrome.Options();
 if (HEADLESS) {
 	chromeOptions.addArguments('--headless');
 }
 
+const firefoxOptions = new firefox.Options();
+if (HEADLESS) {
+	firefoxOptions.addArguments('--headless');
+}
+
 // Setup function to initialize the driver
 async function setupDriver() {
-	let driver = await new Builder()
-		.usingServer(SELENIUM_SERVER_URL)
-		.forBrowser('chrome')
-		.setChromeOptions(chromeOptions)
-		.build();
+	let builder = new Builder().usingServer(SELENIUM_SERVER_URL);
+
+	switch (BROWSER) {
+		case 'firefox':
+			builder = builder.forBrowser('firefox').setFirefoxOptions(firefoxOptions);
+			break;
+		case 'edge':
+			builder = builder.forBrowser('MicrosoftEdge');
+			break;
+		case 'safari':
+			builder = builder.forBrowser('safari');
+			break;
+		case 'chrome':
+		default:
+			builder = builder.forBrowser('chrome').setChromeOptions(chromeOptions);
+			break;
+	}
+
+	let driver = await builder.build();
 	await driver.get(BASE_URL);
 	return driver;
 }
