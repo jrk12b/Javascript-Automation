4313aac92dab78649a68b8be20c39a4b1be8fbdb
Justin Kurdila <justinkurdila@gmail.com>
2024-08-05 11:26:14 -0400

    pushing playwright tests

--- playwright/demoqa/test.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('should have the correct title', async ({ page }) => {
-	await page.goto('https://demoqa.com/');
-
-	const title = await page.title();
-	expect(title).toBe('DEMOQA');
-});
