74644a901855e42ee5dbdab2c4ff73292e486a79
Justin Kurdila <justinkurdila@gmail.com>
2025-09-21 09:44:50 -0400

    fixing cucumber tests

--- features/homePageLinks.feature
+++ features/homePageLinks.feature
@@ -9,11 +9,6 @@ Feature: Home Page Links
     Given I am on the homepage
     When I click the linkedin link
     Then I should land on my linkedin page
-  
-  Scenario: Javascript Automation Github Link
-    Given I am on the homepage
-    When I click the Javascript Automation Github link
-    Then I should land on my Javascript Automation Github page
 
   Scenario: Time of Day Github Link
     Given I am on the homepage
--- features/step_definitions/homePageLinks.js
+++ features/step_definitions/homePageLinks.js
@@ -57,33 +57,6 @@ Then('I should land on my linkedin page', { timeout: 20000 }, async function () 
 	await driver.quit();
 });
 
-When('I click the Javascript Automation Github link', { timeout: 20000 }, async function () {
-	const javascriptAutomationLink = await driver.findElement(
-		By.css('[aria-label="Javascript Automation Git"]')
-	);
-	await driver.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", javascriptAutomationLink);
-	await driver.wait(until.elementIsVisible(javascriptAutomationLink), 5000);
-	await driver.wait(until.elementIsEnabled(javascriptAutomationLink), 5000);
-	await javascriptAutomationLink.click();
-});
-
-Then(
-	'I should land on my Javascript Automation Github page',
-	{ timeout: 20000 },
-	async function () {
-		const { expect } = await import('chai');
-
-		const tabs = await driver.getAllWindowHandles();
-		await driver.switchTo().window(tabs[1]);
-
-		const currentUrl = await driver.getCurrentUrl();
-		expect(currentUrl).to.eq('https://github.com/jrk12b/Javascript-Automation');
-
-		await driver.switchTo().window(tabs[0]);
-		await driver.quit();
-	}
-);
-
 When('I click the Time of Day Github Link', { timeout: 20000 }, async function () {
 	const timeOfDayLink = await driver.findElement(By.css('[aria-label="Time of Day Git"]'));
 	await driver.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", timeOfDayLink);
